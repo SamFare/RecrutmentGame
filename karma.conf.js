12c331bcdef3f2e65b8744077561eee55b16b616
Samuel Fare <samuel.fare@gmail.com>
2019-02-28 17:06:28 +0000

    created new build step to replace build dev

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'lib/*.js',
+      'build/script.js',
       'spec/support/*.js',
       'spec/*.js'
 
